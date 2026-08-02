a896910ae821c37e9f6ebc912e2acbcbe47cd0ad
unknown <etcentura@mail.ru>
2026-08-02 17:33:06 +0300

    soft. optimized out some configs and added dumb bat script to generate modules in one click

---
+++ template_python_generator/python_generator/dumb_bat_script.bat
@@ -0,0 +1,9 @@
+@REM Generating starter pack filters
+
+py template_generator.py -json ../json_configs/custom/isp_gamma_correction_config.json
+
+@REM DONE
+@REM py template_generator.py -json ../json_configs/custom/isp_negative_filter_config.json
+
+@REM DONE
+@REM py template_generator.py -json ../json_configs/custom/isp_cutter_filter_config.json
