945ef1b678c206ee61ab41168ca2c095b7f2dfa8
unknown <etcentura@mail.ru>
2026-08-02 20:04:56 +0300

    soft. started working on universal isp lut

--- template_python_generator/python_generator/dumb_bat_script.bat
+++ template_python_generator/python_generator/dumb_bat_script.bat
@@ -1,9 +1,12 @@
 @REM Generating starter pack filters
 
-py template_generator.py -json ../json_configs/custom/isp_gamma_correction_config.json
+@REM DONE
+@REM py template_generator.py -json ../json_configs/custom/isp_gamma_correction_config.json
 
 @REM DONE
 @REM py template_generator.py -json ../json_configs/custom/isp_negative_filter_config.json
 
 @REM DONE
 @REM py template_generator.py -json ../json_configs/custom/isp_cutter_filter_config.json
+
+py template_generator.py -json ../json_configs/custom/isp_universal_programmable_lut.json
